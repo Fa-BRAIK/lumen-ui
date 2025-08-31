b58e15599c27a55e626c3a51b1e589d79ee98604
Fa-BRAIK <faroukbraik17@gmail.com>
2025-08-31 14:30:39 -0400

    chore: switched checkbox to use Alpine.data instead of Alpine.directive

--- stubs/resources/views/components/checkbox/index.blade.php
+++ stubs/resources/views/components/checkbox/index.blade.php
@@ -5,13 +5,15 @@
     'disabled' => false,
 ])
 
+@php($componentParams = Js::from(compact('defaultChecked', 'disabled')))
+
 @php($attributes = $attributes
     ->twMerge('peer border-input dark:bg-input/30 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground dark:data-[state=checked]:bg-primary data-[state=checked]:border-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive size-4 shrink-0 rounded-[4px] border shadow-xs transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50')
     ->merge([
         'as' => $as,
         'type' => 'button',
         'role' => 'checkbox',
-        'x-checkbox' => Js::from(compact('defaultChecked', 'disabled')),
+        'x-data' => "checkbox($componentParams)",
     ]))
 
 <x-lumen::primitive :attributes="$attributes">

312de72be355bd6bf2d0d3a21e3d988cfe158711
Fa-BRAIK <faroukbraik17@gmail.com>
2025-08-31 14:57:19 -0400

    chore: switched drawer to use Alpine.data instead of Alpine.directive

--- stubs/resources/views/components/drawer/handle.blade.php
+++ stubs/resources/views/components/drawer/handle.blade.php
@@ -6,7 +6,6 @@
     ->twMerge('bg-muted mx-auto mt-4 h-2 w-[100px] shrink-0 rounded-full data-[side=left]:hidden data-[side=right]:hidden')
     ->merge([
         'as' => $as,
-        'x-drawer:handle' => '',
         'data-slot' => 'drawer-handle',
     ]))
 
